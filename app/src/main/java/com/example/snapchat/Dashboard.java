b95498f671ed25320fc77db550c1f32f93a2b250
DiViNe <eesidr5@gmail.com>
2022-02-13 17:43:04 +0500

    login

--- app/src/main/java/com/example/snapchat/Dashboard.java
+++ app/src/main/java/com/example/snapchat/Dashboard.java
@@ -2,23 +2,17 @@ package com.example.snapchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_dashboard);
 
 
     }
-
 }
-
